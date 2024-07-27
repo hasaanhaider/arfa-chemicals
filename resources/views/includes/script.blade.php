64c2d21a7c454c44992746dcea315dd913319abf
hasaanhaider <bejantal456@gmail.com>
2024-07-27 22:11:58 +0500

    setup layout

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -6,3 +6,22 @@
 <script src="{{ asset('assets/libs/lucide/umd/lucide.js') }}"></script>
 <script src="{{ asset('assets/js/tailwick.bundle.js') }}"></script>
 <script src="{{ asset('assets/js/pages/auth-login.init.js') }}"></script>
+
+
+@stack('script')
+
+<script src='assets/libs/choices.js/public/assets/scripts/choices.min.js'></script>
+<script src="assets/libs/%40popperjs/core/umd/popper.min.js"></script>
+<script src="assets/libs/tippy.js/tippy-bundle.umd.min.js"></script>
+<script src="assets/libs/simplebar/simplebar.min.js"></script>
+<script src="assets/libs/prismjs/prism.js"></script>
+<script src="assets/libs/lucide/umd/lucide.js"></script>
+<script src="assets/js/tailwick.bundle.js"></script>
+<!--apexchart js-->
+<script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+
+<!--dashboard ecommerce init js-->
+<script src="assets/js/pages/dashboards-ecommerce.init.js"></script>
+
+<!-- App js -->
+<script src="assets/js/app.js"></script>
